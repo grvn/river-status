b252097cac4b9a7d66e234eb032ceb0a026fcb06
grvn <bigricke@hotmail.com>
2025-03-08 15:12:06 +0100

    simplify serialize

--- src/seat.rs
+++ src/seat.rs
@@ -1,11 +1,13 @@
-use wayland_client::protocol::wl_seat::WlSeat;
-use wayland_client::Proxy;
-use serde::Serialize;
 use crate::protocols::river_status_unstable::v1::zriver_seat_status_v1::ZriverSeatStatusV1;
+use serde::Serialize;
+use wayland_client::protocol::wl_seat::WlSeat;
 
+#[derive(Serialize)]
 pub struct Seat {
   pub name: String,
+  #[serde(skip)]
   pub status: Option<ZriverSeatStatusV1>,
+  #[serde(skip)]
   pub wlseat: WlSeat,
 }
 
@@ -16,14 +18,5 @@ impl Seat {
       status: None,
       wlseat,
     }
-  }
-}
-
-impl Serialize for Seat {
-  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-  where
-    S: serde::Serializer,
-  {
-    serializer.serialize_str(&self.wlseat.id().to_string())
   }
 }
