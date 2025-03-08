849aa3b4a8fcb3a6e2aa30fce94e7ab38084b5f9
grvn <bigricke@hotmail.com>
2025-03-08 11:22:32 +0100

    make the compiler a pain in my ass

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+#![deny(clippy::expect_used)]
+#![deny(clippy::panic)]
+#![deny(clippy::unwrap_used)]
+#![deny(unused_must_use)]
+
+#![deny(clippy::await_holding_lock)]
+#![deny(clippy::needless_pass_by_value)]
+#![deny(clippy::todo)]
+#![deny(clippy::trivially_copy_pass_by_ref)]
+#![deny(clippy::unimplemented)]
+#![deny(clippy::unreachable)]
+#![deny(warnings)]
+#![warn(unused_extern_crates)]
+
 pub mod client;
 pub mod flags;
 pub mod output;
