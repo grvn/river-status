098b324b49e351d08d8defbe423fdbd35f7617b2
grvn <bigricke@hotmail.com>
2025-03-10 12:38:23 +0100

    use serde_with for readablity

--- src/output.rs
+++ src/output.rs
@@ -1,20 +1,21 @@
 // output as in displays|screens|monitors
 use crate::protocols::river_status_unstable::v1::zriver_output_status_v1::ZriverOutputStatusV1;
 use serde::Serialize;
+use serde_with::skip_serializing_none;
 use wayland_client::protocol::wl_output::WlOutput;
 
+#[skip_serializing_none]
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Output {
   pub focused: bool,
-  #[serde(skip_serializing_if = "Option::is_none")]
   pub focused_tags: Option<u32>,
+  pub layout: Option<String>,
   pub name: String,
   #[serde(skip_serializing_if = "Vec::is_empty")]
   pub occupied_tags: Vec<u32>,
   #[serde(skip)]
   pub status: Option<ZriverOutputStatusV1>,
-  #[serde(skip_serializing_if = "Option::is_none")]
   pub urgent_tags: Option<u32>,
   #[serde(skip)]
   pub wloutput: WlOutput,
@@ -25,6 +26,7 @@ impl Output {
     Self {
       focused: false,
       focused_tags: None,
+      layout: None,
       name,
       occupied_tags: vec![],
       status: None,
